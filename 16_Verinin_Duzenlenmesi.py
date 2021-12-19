40b80d9ea46025b7db45b5acc88596267196aa96
miyigun <miyigun@hotmail.com>
2021-12-19 16:30:58 +0300

    19-12-2021

---
+++ 16_Verinin_Duzenlenmesi.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
+
+#python'da uyarıları kapatalım
+import warnings
+warnings.filterwarnings("ignore")
+
+#veriyi içeri aktaracağız
+veri=read_csv("olimpiyatlar.csv")
